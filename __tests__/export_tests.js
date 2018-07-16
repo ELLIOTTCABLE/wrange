3d05b6a142d4b20f844885fe32b8ced588e54c64
ELLIOTTCABLE <me@ell.io>
2018-07-16 09:22:14 -0500

    (- fix style) Re-indent. WTF, vim.

--- __tests__/export_tests.js
+++ __tests__/export_tests.js
@@ -1,18 +1,18 @@
 const wrange = require('../src/exports.js');
 
 describe("Exports", () => {
-  test("exists", () => {
-    expect(wrange).toBeDefined()
-  })
+   test("exists", () => {
+      expect(wrange).toBeDefined()
+   })
 
-  describe("Person", () => {
-    test("exists", () => {
-      expect(wrange.Person).toBeDefined()
-    })
+   describe("Person", () => {
+      const Person = wrange.Person
+      test("exists", () => {
+         expect(Person).toBeDefined()
+      })
 
-    test("can be created from JS", () => {
-      expect(wrange.Person.create).toBeInstanceOf(Function)
-    })
-  })
+      test("exports a creation-function in JS", () => {
+         expect(Person.create).toBeInstanceOf(Function)
+      })
+   })
 }) // describe Exports
-
--- __tests__/wrange_tests.ml
+++ __tests__/wrange_tests.ml
@@ -2,7 +2,7 @@ open Jest
 
 let _ =
 describe "Data model" begin fun ()->
-  test "Blah" Expect.(fun ()->
-    expect (1 + 2) |> (toBe 3)
-  )
+   test "Blah" Expect.(fun ()->
+      expect (1 + 2) |> (toBe 3)
+   )
 end
