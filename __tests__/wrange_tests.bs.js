030484de0aa7f6e198829d10956ab99122bd850e
ELLIOTTCABLE <me@ell.io>
2018-07-17 16:23:30 -0500

    (new tests) Excercise creation of Person

--- __tests__/wrange_tests.bs.js
+++ __tests__/wrange_tests.bs.js
@@ -1,11 +1,33 @@
 'use strict';
 
 var Jest = require("@glennsl/bs-jest/src/jest.js");
+var Person = require("../src/person.bs.js");
+var Caml_builtin_exceptions = require("bs-platform/lib/js/caml_builtin_exceptions.js");
+
+function make_birthday_exn(iso8601) {
+  var date = new Date(iso8601);
+  var flo = date.valueOf();
+  if (isNaN(flo)) {
+    throw [
+          Caml_builtin_exceptions.failure,
+          "Unparsable date"
+        ];
+  } else {
+    return date;
+  }
+}
 
 describe("Data model", (function () {
-        return Jest.test("Blah", (function () {
-                      return Jest.Expect[/* toBe */2](3, Jest.Expect[/* expect */0](3));
-                    }));
+        describe("Person", (function () {
+                return Jest.test("can be created", (function () {
+                              var person = Person.create("Wittig", "Kachel", /* Female */1, /* Yellow */4, make_birthday_exn("1989-01-25"));
+                              return Jest.Expect[/* toThrow */18](Jest.Expect[/* not_ */23](Jest.Expect[/* expect */0]((function () {
+                                                    return person;
+                                                  }))));
+                            }));
+              }));
+        return /* () */0;
       }));
 
+exports.make_birthday_exn = make_birthday_exn;
 /*  Not a pure module */
--- __tests__/wrange_tests.ml
+++ __tests__/wrange_tests.ml
@@ -1,8 +1,20 @@
 open Jest
 
+let make_birthday_exn iso8601 =
+   let date = Js.Date.fromString iso8601 in
+   let flo = Js.Date.valueOf date in
+   if not @@ Js.Float.isNaN flo
+   then date
+   else failwith "Unparsable date"
+
+
 let _ =
 describe "Data model" begin fun ()->
-   test "Blah" Expect.(fun ()->
-      expect (1 + 2) |> (toBe 3)
-   )
+   describe "Person" begin fun ()->
+      test "can be created" Expect.(fun ()->
+         let person = Person.create ~first_name:"Kachel" ~last_name:"Wittig" ~gender:Person.Female
+         ~favourite_colour:Person.Yellow ~birthday:(make_birthday_exn "1989-01-25") in
+         expect (fun _-> person) |> not_ |> toThrow
+      )
+   end
 end
