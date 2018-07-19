d35b2bbacec9b7a1c1261e06d36daeb1374fae1b
ELLIOTTCABLE <me@ell.io>
2018-07-18 23:44:00 -0500

    (- NF server) Iterate towards a person-set server

--- src/server.bs.js
+++ src/server.bs.js
@@ -1,32 +1,117 @@
 'use strict';
 
 var Curry = require("bs-platform/lib/js/curry.js");
+var Js_exn = require("bs-platform/lib/js/js_exn.js");
+var $$String = require("bs-platform/lib/js/string.js");
 var Express = require("bs-express/src/Express.js");
+var Js_json = require("bs-platform/lib/js/js_json.js");
+var Process = require("process");
 
-function makeSuccessJson() {
+function make_success() {
   var json = { };
   json["success"] = true;
   return json;
 }
 
+function make_failure() {
+  var json = { };
+  json["status"] = "failure";
+  return json;
+}
+
+function getDictString(dict, key) {
+  var match = dict[key];
+  if (match !== undefined) {
+    return Js_json.decodeString(match);
+  }
+  
+}
+
+function $great$great(f, g, x) {
+  return Curry._1(g, Curry._1(f, x));
+}
+
+function logRequest(next, req) {
+  var ip$prime = Express.Request[/* ip */10](req);
+  var meth = Express.Request[/* methodRaw */13](req);
+  var url = Express.Request[/* path */16](req);
+  var match = Express.Request[/* bodyText */5](req);
+  var text = match !== undefined ? $$String.concat("", /* :: */[
+          ": \"",
+          /* :: */[
+            match,
+            /* :: */[
+              "\"",
+              /* [] */0
+            ]
+          ]
+        ]) : "";
+  console.log("" + (String(ip$prime) + (" ~ " + (String(meth) + (" " + (String(url) + ("" + (String(text) + ""))))))));
+  return Curry._1(next, Express.Next[/* middleware */0]);
+}
+
+function addPerson(_, req) {
+  var match = Express.Request[/* bodyText */5](req);
+  if (match !== undefined) {
+    var partial_arg = match;
+    var partial_arg$1 = Express.Response[/* sendString */2];
+    return (function (param) {
+        return partial_arg$1(partial_arg, param);
+      });
+  } else {
+    var partial_arg$2 = make_failure(/* () */0);
+    var partial_arg$3 = Express.Response[/* sendJson */3];
+    var partial_arg$4 = Express.Response[/* status */9](/* BadRequest */19);
+    return (function (param) {
+        var param$1 = Curry._1(partial_arg$4, param);
+        return partial_arg$3(partial_arg$2, param$1);
+      });
+  }
+}
+
+function onListen(err) {
+  var exit = 0;
+  var val;
+  try {
+    val = err;
+    exit = 1;
+  }
+  catch (raw_exn){
+    var exn = Js_exn.internalToOCamlException(raw_exn);
+    if (exn[0] === Js_exn.$$Error) {
+      console.log(exn[1]);
+      Process.exit(1);
+      return /* () */0;
+    } else {
+      throw exn;
+    }
+  }
+  if (exit === 1) {
+    console.log("Listening at http://127.0.0.1:3000");
+    return /* () */0;
+  }
+  
+}
+
+var api = Express.Router[/* make */15](undefined, undefined, undefined, /* () */0);
+
+Express.Router[/* post */7](api, "/records", Express.Middleware[/* from */5](addPerson));
+
 var app = Express.App[/* make */15](/* () */0);
 
-Express.App[/* get */4](app, "/method", Express.Middleware[/* from */5]((function (next, req) {
-            var s = Express.Request[/* httpMethod */14](req);
-            if (s !== 0) {
-              console.log(s);
-              return Curry._1(next, Express.Next[/* route */1]);
-            } else {
-              var partial_arg = makeSuccessJson(/* () */0);
-              var partial_arg$1 = Express.Response[/* sendJson */3];
-              return (function (param) {
-                  return partial_arg$1(partial_arg, param);
-                });
-            }
-          })));
+Express.App[/* use */0](app, Express.Middleware[/* text */1](undefined, undefined, undefined, undefined, /* () */0));
 
-Express.App[/* listen */19](app, 3000, undefined, /* () */0);
+Express.App[/* use */0](app, Express.Middleware[/* from */5](logRequest));
 
-exports.makeSuccessJson = makeSuccessJson;
-exports.app = app;
-/* app Not a pure module */
+Express.App[/* useRouterOnPath */18](app, "/v1", api);
+
+Express.App[/* listen */19](app, 3000, onListen, /* () */0);
+
+exports.make_success = make_success;
+exports.make_failure = make_failure;
+exports.getDictString = getDictString;
+exports.$great$great = $great$great;
+exports.logRequest = logRequest;
+exports.addPerson = addPerson;
+exports.onListen = onListen;
+/* api Not a pure module */
--- src/server.ml
+++ src/server.ml
@@ -1,20 +1,52 @@
 open Express
 
-let makeSuccessJson () =
+let make_success () =
   let json = Js.Dict.empty () in
   Js.Dict.set json "success" (Js.Json.boolean true); Js.Json.object_ json
 
+let make_failure () =
+  let json = Js.Dict.empty () in
+  Js.Dict.set json "status" (Js.Json.string "failure"); Js.Json.object_ json
 
-let app = App.make ()
+let getDictString dict key =
+  match Js.Dict.get dict key with
+  | ((Some (json))[@explicit_arity ]) -> Js.Json.decodeString json
+  | _ -> None
+
+let (>>) f g x = x |> f |> g
+
+
+let logRequest next req =
+   let open Request in
+   let ip' = ip req and meth = methodRaw req and url = path req
+   and text = begin match bodyText req with
+   | Some t -> String.concat "" [": \""; t; "\""]
+   | None -> ""
+   end in
+   Js.log {j|$(ip') ~ $meth $(url)$text|j};
+   next Next.middleware
+
+let addPerson next req (* res *) =
+   match Request.bodyText req with
+   | None ->
+      Response.status Response.StatusCode.BadRequest
+      >> Response.sendJson (make_failure ())
+   | Some text ->
+         Response.sendString text
+
+
+let onListen err = match err with
+  | exception Js.Exn.Error e -> Js.log e; Node.Process.exit 1
+  | _ -> Js.log "Listening at http://127.0.0.1:3000"
+
 
 let _ =
-  App.get app ~path:"/method" @@
-  Middleware.from begin fun next req ->
-     let open Request in
-     match httpMethod req with
-     | Get -> Response.sendJson @@ makeSuccessJson ()
-     | s -> Js.log s; next Next.route
-  end
+   let api = Router.make () in
 
-let _ =
-   App.listen app ~port:3000 ()
+   Router.post api ~path:"/records" @@ Middleware.from addPerson;
+
+   let app = App.make () in
+   app |. App.use (Middleware.text ());
+   app |. App.use (Middleware.from logRequest);
+   app |. App.useRouterOnPath ~path:"/v1" api;
+   app |. App.listen ~port:3000 ~onListen ()
