cdfcdc5905694494708b8bccf59d6b344fd77c4f
ELLIOTTCABLE <git@ell.io>
2019-06-12 06:17:36 -0500

    (- new doc CLI) Clean up and elaborate on documentation for print command

--- src/cli.bs.js
+++ src/cli.bs.js
@@ -2,14 +2,56 @@
 
 var List = require("bs-platform/lib/js/list.js");
 var $$Array = require("bs-platform/lib/js/array.js");
+var Block = require("bs-platform/lib/js/block.js");
 var Curry = require("bs-platform/lib/js/curry.js");
 var Person = require("./person.bs.js");
+var Printf = require("bs-platform/lib/js/printf.js");
 var Server = require("./server.bs.js");
 var Cmdliner = require("@elliottcable/bs-cmdliner/src/cmdliner.bs.js");
 var PersonSet = require("./personSet.bs.js");
 
 process.argv.shift()
 ;
+
+var sort_keys = /* :: */[
+  /* tuple */[
+    "last",
+    /* Last */847656566
+  ],
+  /* :: */[
+    /* tuple */[
+      "first",
+      /* First */332064784
+    ],
+    /* :: */[
+      /* tuple */[
+        "gender",
+        /* Gender */-741283999
+      ],
+      /* :: */[
+        /* tuple */[
+          "birthday",
+          /* Birthday */-243010339
+        ],
+        /* [] */0
+      ]
+    ]
+  ]
+];
+
+var sort_orders = /* :: */[
+  /* tuple */[
+    "ascending",
+    /* Ascending */-800424520
+  ],
+  /* :: */[
+    /* tuple */[
+      "descending",
+      /* Descending */235215128
+    ],
+    /* [] */0
+  ]
+];
 
 function start(port, files) {
   var set = PersonSet.create(/* () */0);
@@ -34,53 +76,13 @@ function print(sorts, files) {
               }), people);
 }
 
-var sort_key = Cmdliner.Arg[/* enum */33](/* :: */[
-      /* tuple */[
-        "last",
-        /* Last */847656566
-      ],
-      /* :: */[
-        /* tuple */[
-          "first",
-          /* First */332064784
-        ],
-        /* :: */[
-          /* tuple */[
-            "gender",
-            /* Gender */-741283999
-          ],
-          /* :: */[
-            /* tuple */[
-              "birthday",
-              /* Birthday */-243010339
-            ],
-            /* [] */0
-          ]
-        ]
-      ]
-    ]);
-
-var sort_order = Cmdliner.Arg[/* enum */33](/* :: */[
-      /* tuple */[
-        "ascending",
-        /* Ascending */-800424520
-      ],
-      /* :: */[
-        /* tuple */[
-          "descending",
-          /* Descending */235215128
-        ],
-        /* [] */0
-      ]
-    ]);
-
 var partial_arg = Cmdliner.Arg[/* string */32];
 
 var partial_arg$1 = Cmdliner.Arg[/* pos_all */17];
 
 var files$prime = Cmdliner.Arg[/* & */9](Cmdliner.Arg[/* non_empty */22], Cmdliner.Arg[/* & */9]((function (param) {
             return partial_arg$1(partial_arg, /* [] */0, param);
-          }), Cmdliner.Arg[/* info */8](undefined, "FILE", undefined, undefined, /* [] */0)));
+          }), Cmdliner.Arg[/* info */8](undefined, "FILE", "The path to a text-file containing a set of person-records, in a supported format. (See $(b,FORMATS), below.) May be specified multiple times; the records in later files will be merged into the set implied by earlier ones with preference to later entries.", undefined, /* [] */0)));
 
 var partial_arg$2 = Cmdliner.Arg[/* int */27];
 
@@ -88,7 +90,7 @@ var partial_arg$3 = Cmdliner.Arg[/* opt */14];
 
 var port$prime = Cmdliner.Arg[/* & */9](Cmdliner.Arg[/* value */20], Cmdliner.Arg[/* & */9]((function (param) {
             return partial_arg$3(undefined, partial_arg$2, 3000, param);
-          }), Cmdliner.Arg[/* info */8](undefined, "PORT", "Provide a REST interface on $(docv)", undefined, /* :: */[
+          }), Cmdliner.Arg[/* info */8](undefined, "PORT", "Bind to connections on $(docv).", undefined, /* :: */[
               "p",
               /* :: */[
                 "port",
@@ -96,13 +98,47 @@ var port$prime = Cmdliner.Arg[/* & */9](Cmdliner.Arg[/* value */20], Cmdliner.Ar
               ]
             ])));
 
+var sort_key = Cmdliner.Arg[/* enum */33](sort_keys);
+
+var sort_key_doc = Cmdliner.Arg[/* doc_alts_enum */45](undefined, sort_keys);
+
+var sort_order = Cmdliner.Arg[/* enum */33](sort_orders);
+
+var sort_order_doc = Cmdliner.Arg[/* doc_alts_enum */45](undefined, sort_orders);
+
+var doc = Curry._3(Printf.sprintf(/* Format */[
+          /* String_literal */Block.__(11, [
+              "Specify the single sorting constraint $(docv) to apply to the output. May be specified more than once, in which case the sorts are applied lexicographically, in corresponding order. The constraint $(docv) must be specified as a comma-separated pair of ",
+              /* String */Block.__(2, [
+                  /* No_padding */0,
+                  /* String_literal */Block.__(11, [
+                      "; where $(b,KEY) is ",
+                      /* String */Block.__(2, [
+                          /* No_padding */0,
+                          /* String_literal */Block.__(11, [
+                              ", and $(b,ORDER) is ",
+                              /* String */Block.__(2, [
+                                  /* No_padding */0,
+                                  /* Char_literal */Block.__(12, [
+                                      /* "." */46,
+                                      /* End_of_format */0
+                                    ])
+                                ])
+                            ])
+                        ])
+                    ])
+                ])
+            ]),
+          "Specify the single sorting constraint $(docv) to apply to the output. May be specified more than once, in which case the sorts are applied lexicographically, in corresponding order. The constraint $(docv) must be specified as a comma-separated pair of %s; where $(b,KEY) is %s, and $(b,ORDER) is %s."
+        ]), Cmdliner.Arg[/* doc_quote */43]("$(b,KEY,ORDER)"), sort_key_doc, sort_order_doc);
+
 var partial_arg$4 = Cmdliner.Arg[/* pair */39](undefined, sort_key, sort_order);
 
 var partial_arg$5 = Cmdliner.Arg[/* opt_all */15];
 
 var sorts$prime = Cmdliner.Arg[/* & */9](Cmdliner.Arg[/* value */20], Cmdliner.Arg[/* & */9]((function (param) {
             return partial_arg$5(undefined, partial_arg$4, /* [] */0, param);
-          }), Cmdliner.Arg[/* info */8](undefined, "SORTSPEC", "DOCME", undefined, /* :: */[
+          }), Cmdliner.Arg[/* info */8](undefined, "SORTSPEC", doc, undefined, /* :: */[
               "sort",
               /* [] */0
             ])));
@@ -116,7 +152,7 @@ var start$prime_001 = Curry.app(Cmdliner.Term[/* info */14], [
       undefined,
       undefined,
       undefined,
-      "Start a REST server",
+      "Start a REST server publishing given person-records",
       undefined,
       "start"
     ]);
@@ -135,7 +171,7 @@ var print$prime_001 = Curry.app(Cmdliner.Term[/* info */14], [
       undefined,
       undefined,
       undefined,
-      "Sort, and display, the contents of the given person-records",
+      "Sort, and display, the contents of person-records stored in files",
       undefined,
       "print"
     ]);
@@ -184,10 +220,10 @@ var commands = /* :: */[
 
 Cmdliner.Term[/* eval_choice */17](undefined, undefined, undefined, undefined, undefined, default$prime, commands);
 
+exports.sort_keys = sort_keys;
+exports.sort_orders = sort_orders;
 exports.start = start;
 exports.print = print;
-exports.sort_key = sort_key;
-exports.sort_order = sort_order;
 exports.files$prime = files$prime;
 exports.port$prime = port$prime;
 exports.sorts$prime = sorts$prime;
--- src/cli.ml
+++ src/cli.ml
@@ -4,6 +4,12 @@ open Cmdliner
 [%%raw
    "process.argv.shift()"]
 
+let sort_keys =
+   [("last", `Last); ("first", `First); ("gender", `Gender); ("birthday", `Birthday)]
+
+
+let sort_orders = [("ascending", `Ascending); ("descending", `Descending)]
+
 let start port files =
    let set = PersonSet.create () in
    files
@@ -25,35 +31,49 @@ let print sorts files =
 
 
 (* CLI declarations *)
-let sort_key : Person.sort_key Arg.conv =
-   Arg.enum
-      [("last", `Last); ("first", `First); ("gender", `Gender); ("birthday", `Birthday)]
+
+let files' =
+   let doc =
+      "The path to a text-file containing a set of person-records, in a supported \
+       format. (See $(b,FORMATS), below.) May be specified multiple times; the records \
+       in later files will be merged into the set implied by earlier ones with \
+       preference to later entries."
+   in
+   Arg.(non_empty & pos_all string [] & info [] ~docv:"FILE" ~doc)
 
 
-let sort_order : Person.sort_order Arg.conv =
-   Arg.enum [("ascending", `Ascending); ("descending", `Descending)]
-
-
-let files' = Arg.(non_empty & pos_all string [] & info [] ~docv:"FILE")
+(* TODO: Document FORMATS. *)
 
 let port' =
-   let doc = "Provide a REST interface on $(docv)" in
+   let doc = "Bind to connections on $(docv)." in
    Arg.(value & opt int 3000 & info ["p"; "port"] ~docv:"PORT" ~doc)
 
 
 let sorts' =
-   let doc = "DOCME" in
    let open Arg in
+   let sort_key = enum sort_keys in
+   let sort_key_doc = doc_alts_enum sort_keys in
+   let sort_order = enum sort_orders in
+   let sort_order_doc = doc_alts_enum sort_orders in
+   let doc =
+      Printf.sprintf
+         "Specify the single sorting constraint $(docv) to apply to the output. May be \
+          specified more than once, in which case the sorts are applied \
+          lexicographically, in corresponding order. The constraint $(docv) must be \
+          specified as a comma-separated pair of %s; where $(b,KEY) is %s, and \
+          $(b,ORDER) is %s."
+         (doc_quote "$(b,KEY,ORDER)") sort_key_doc sort_order_doc
+   in
    value & opt_all (pair sort_key sort_order) [] & info ["sort"] ~docv:"SORTSPEC" ~doc
 
 
 let start' =
-   let doc = "Start a REST server" in
+   let doc = "Start a REST server publishing given person-records" in
    (Term.(const start $ port' $ files'), Term.info ~doc "start")
 
 
 let print' =
-   let doc = "Sort, and display, the contents of the given person-records" in
+   let doc = "Sort, and display, the contents of person-records stored in files" in
    (Term.(const print $ sorts' $ files'), Term.info ~doc "print")
 
 
